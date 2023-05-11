c93decf18814f2c1c312024e954b31509d96bbad
ramachell <mjh1548@gamil.com>
2023-05-11 15:32:17 +0900

    뭔지모르지만 2수정됐대

--- Step01_DataType/src/test/main/MainClass02.java
+++ Step01_DataType/src/test/main/MainClass02.java
@@ -7,13 +7,13 @@ public class MainClass02 {
 //		3. 영어점수 100을 eng 변수에 담기
 //		4. 국어 점수와 영어점수 평균 구해서 avg 변수에 담기
 //		5. 콘솔창에 avg 변수에 담긴거 출력
-		
+
 		System.out.println("main 메소드가 시작되었습니다.");
 		int d = 34;
 		int kor = 95;
 		int eng = 100;
-		double avg = (kor+eng)/2d;
+		double avg = (kor + eng) / 2d;
 		System.out.println(avg);
-		
+
 	}
 }
