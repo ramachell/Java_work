75d803f554e51848fc1d1b6628b455d9fa23c054
ramachell <mjh1548@gamil.com>
2023-05-15 16:50:41 +0900

    Step17 MainClass10 좀 편하게 기능 변경함
    순서가 좀 잘못됐는데 DBConncet.java는 이 커밋에서 추가된거임

--- Step17_JDBC/src/test/main/MainClass10.java
+++ Step17_JDBC/src/test/main/MainClass10.java
@@ -1,10 +1,10 @@
 package test.main;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 import test.mypac.MemberDto;
+import test.util.DBConnect;
 
 public class MainClass10 {
 	public static void main(String[] args) {
@@ -22,21 +22,11 @@ public class MainClass10 {
 	}
 
 	public static void update(MemberDto dto) {
-		Connection conn = null;
-
-		try {
-			// 오라클 드라이버 로딩
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			// 접속할 DB 의 정보 @아이피주소:port번호:db이름
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			// 계정 비밀번호를 이용해서 Connection 객체의 참조값 얻어오기
-			conn = DriverManager.getConnection(url, "scott", "tiger");
-			// 예외가 발생하지 않고 여기까지 실행순서가 내려오면 접속 성공이다.
-			System.out.println("Oracle DB 접속 성공");
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
-
+		Connection conn = new DBConnect().getconn();
+		// 아래꺼를 한줄로 줄인것
+//		Connection conn;
+//		DBConnect db = new DBConnect();
+//		conn = db.getconn();
 		//
 		int num = dto.getNum();
 		String name = dto.getName();
